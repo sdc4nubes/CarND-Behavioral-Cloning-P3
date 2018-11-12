7c827bf5ca7841c08b3d9e9850135b044d2e17a5
rons-git <ron@cancunron.com>
2018-11-11 20:40:09 -0500

    update model

--- model.py
+++ model.py
@@ -87,7 +87,7 @@ def data_augmentation(images, angles):
     for image, angle in zip(images, angles):
 # adjust image attributes to fit model
         image = image[60:130,:,:]
-        image = cv2.resize(image, (70, 120), interpolation = cv2.INTER_AREA)
+        image = cv2.resize(image, (50, 120), interpolation = cv2.INTER_AREA)
         augmented_images.append(image)
         augmented_angles.append(angle)
 # flip
@@ -115,7 +115,7 @@ def get_fullName(fName):
 
 def VGG():
     model = Sequential()
-    model.add(Lambda(lambda x: (x - 128) / 128, input_shape=(120, 70, 1)))
+    model.add(Lambda(lambda x: (x - 128) / 128, input_shape=(120, 50, 1)))
     model.add(Conv2D(8, 5, 5, activation='relu'))
     model.add(MaxPooling2D())
     model.add(Conv2D(12, 5, 5, activation='relu'))
